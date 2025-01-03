9c2b06c206a29ac6867963fbd8051109aef53e4c
hannbh <hanna.b.harding@gmail.com>
2025-01-03 19:12:49 +0000

    index and entry pages

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
+import markdown2
 
 from . import util
-
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+def entry(request, title):
+    content = util.get_entry(title)
+    if content != None:
+        content = markdown2.markdown(content)
+    return render(request, "encyclopedia/entry.html", {
+        "title": title,
+        "content": content
+    })
