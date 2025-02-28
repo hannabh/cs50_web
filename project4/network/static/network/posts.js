e10da26c9f78e3e2383a03968f14a0f1c0e74b0a
hannbh <hanna.b.harding@gmail.com>
2025-02-28 12:33:03 +0000

    update edit post feature

--- project4/network/static/network/posts.js
+++ project4/network/static/network/posts.js
@@ -1,5 +1,32 @@
+function editPost(postContentWrapper) {
+    let postContent = postContentWrapper.querySelector('.post-content');
+
+    // replace post content with textarea and Save button
+    postContentWrapper.innerHTML = `
+    <textarea id="edit-text" class="form-control" rows="3">${postContent.textContent}</textarea>
+    <button class="btn btn-primary" id="save-button">Save</button>
+    `;
+    // add event listener to Save button
+    postContentWrapper.querySelector('#save-button').addEventListener('click', function() {
+        savePost(postContentWrapper);
+    });
+}
+
 function savePost(postContentWrapper) {
-    alert('saving post')  // TODO
+    // TODO: actually update post using API
+
+    let editedContent = postContentWrapper.querySelector('#edit-text').value;
+
+    postContentWrapper.innerHTML = `
+    <p class="post-content">${editedContent}</p>
+    <a class="edit-link" href="#">Edit</a>
+    `
+
+    // reattach event listener for the new edit link
+    postContentWrapper.querySelector('.edit-link').addEventListener('click', function(event) {
+        event.preventDefault();
+        editPost(postContentWrapper);
+    });
 }
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -9,18 +36,10 @@ document.addEventListener('DOMContentLoaded', () => {
             // prevent link from reloading page
             event.preventDefault() 
 
-            // Find the post content of the clicked edit link
+            // find the post content wrapper of the clicked edit link
             let postContentWrapper = event.target.closest('.post-content-wrapper');
-            let postContent = postContentWrapper.querySelector('.post-content');
 
-            // replace post content with textarea and Save button
-            postContentWrapper.innerHTML = `
-            <textarea id="edit-text" class="form-control" rows="3">${postContent.textContent}</textarea>
-            <button class="btn btn-primary" id="save-button">Save</button>
-            `;
-
-            // add event listener for Save button
-            postContentWrapper.querySelector('#save-button').onclick = savePost;
+            editPost(postContentWrapper);
         }
     });
-});
+})
